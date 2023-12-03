680388b79d1f9ecd2829d02f4873bff0f2192056
Easton Seidel <eseidel1357@gmail.com>
2023-12-03 12:22:00 -0800

    Created day 2 file and input txt

---
+++ day 2/part 1/day_2.py
@@ -0,0 +1,23 @@
+"""
+Day 2 part 1
+By: Easton Seidel
+"""
+
+
+def main():
+    possible = []
+    impossible = []
+    max_values = {'Red': 12, 'green': 13, 'blue': 14}
+
+    with open("input.txt") as i:
+        lines = i.readlines()
+
+    for i in lines:
+        hands = i.split(":")[1].split(";")
+
+        for j in hands:
+            j = j.split(" ")
+
+
+if __name__ == "__main__":
+    main()
