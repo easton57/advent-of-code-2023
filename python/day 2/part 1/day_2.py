0d34482dc0ff95e3f22b5e9a09660b0f1bc324a9
Easton Seidel <eseidel1357@gmail.com>
2023-12-03 12:50:45 -0800

    Didn't read the instructions close enough. This one works.

--- python/day 2/part 1/day_2.py
+++ python/day 2/part 1/day_2.py
@@ -9,20 +9,26 @@ def main():
     impossible = []
     max_values = {'red': 12, 'green': 13, 'blue': 14}
 
+    # Read our input
     with open("input.txt") as i:
         lines = i.readlines()
 
+    # Find what's possible
     for i in lines:
         hands = i.split(":")[1].split(";")
 
         for j in hands:
+            if i in impossible:
+                break
+
+            temp_num = 0
             j = j.split(" ")
             j.remove('')
 
             for k in j:
                 try:
                     temp_num = int(k)
-                except:
+                except ValueError:
                     # Not a number!
                     if "blue" in k:
                         if temp_num > max_values.get("blue"):
@@ -40,8 +46,17 @@ def main():
         if i not in impossible:
             possible.append(i)
 
+    # Get our value
+    total = 0
+
+    for i in possible:
+        i = i.split(":")[0].split(" ")
+
+        total += int(i[1])
+
+    # Write the answer out to a file
     with open("day_2_1_output.txt", "w") as o:
-        o.write(str(len(possible)))
+        o.write(str(total))
 
 
 if __name__ == "__main__":
