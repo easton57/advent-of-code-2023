65bccef2e6fec8bc6d2ab547cffab85c2b89c6d2
Easton Seidel <eseidel1357@gmail.com>
2023-12-03 14:03:00 -0800

    created template

---
+++ python/template.py
@@ -0,0 +1,18 @@
+"""
+Day 3 advent of code
+by: Easton Seidel
+"""
+
+
+def main():
+    # Read our input
+    with open("../input.txt") as i:
+        lines = i.readlines()
+
+    # Write the answer out to a file
+    with open("day_x_output.txt", "w") as o:
+        o.write("Some value")
+
+
+if __name__ == "__main__":
+    main()
