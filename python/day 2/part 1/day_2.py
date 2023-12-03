40b7d9ab2dc091fc295a7fbc6b35d74001454503
Easton Seidel <eseidel1357@gmail.com>
2023-12-03 12:42:07 -0800

    Attempt 1 finished

--- python/day 2/part 1/day_2.py
+++ python/day 2/part 1/day_2.py
@@ -7,7 +7,7 @@ By: Easton Seidel
 def main():
     possible = []
     impossible = []
-    max_values = {'Red': 12, 'green': 13, 'blue': 14}
+    max_values = {'red': 12, 'green': 13, 'blue': 14}
 
     with open("input.txt") as i:
         lines = i.readlines()
@@ -17,8 +17,31 @@ def main():
 
         for j in hands:
             j = j.split(" ")
+            j.remove('')
 
+            for k in j:
+                try:
+                    temp_num = int(k)
+                except:
+                    # Not a number!
+                    if "blue" in k:
+                        if temp_num > max_values.get("blue"):
+                            impossible.append(i)
+                            break
+                    elif "red" in k:
+                        if temp_num > max_values.get("red"):
+                            impossible.append(i)
+                            break
+                    else:  # Green
+                        if temp_num > max_values.get("green"):
+                            impossible.append(i)
+                            break
 
+        if i not in impossible:
+            possible.append(i)
+
+    with open("day_2_1_output.txt", "w") as o:
+        o.write(str(len(possible)))
 
 
 if __name__ == "__main__":
