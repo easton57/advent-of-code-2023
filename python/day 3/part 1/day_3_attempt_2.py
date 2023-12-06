5d674da64b4ee3031599512030db6f0cefe038eb
Easton Seidel <eseidel1357@gmail.com>
2023-12-05 19:24:38 -0800

    It's horrible but I got the right answer

--- python/day 3/part 1/day_3_attempt_2.py
+++ python/day 3/part 1/day_3_attempt_2.py
@@ -3,21 +3,19 @@ Day 3 advent of code
 By: Easton Seidel
 """
 
-sum = 0
-lines = None
-
 
 def remove_items(test_list, item): 
     # remove the item for all its occurrences 
     c = test_list.count(item) 
-    for i in range(c): 
-        test_list.remove(item) 
+    for i in range(c):
+        test_list.remove(item)
     return test_list 
 
 
 def main():
-    global sum
-    global lines
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\n']
+    sum = 0
+    e = None
     
     # Read our input
     with open("../input.txt") as i:
@@ -27,8 +25,9 @@ def main():
     for i in range(len(lines)):
         # Pull current line stats
         print(f"\nStep {i + 1}\nCurrent working line: {lines[i]}")
+        j = 0
         
-        for j in range(len(lines[i])):
+        while j < len(lines[i]):
             symbol_array = []
             
             # A single num
@@ -40,15 +39,14 @@ def main():
                 e = j + 1
                 # A second num
                 try:
-                    int(lines[i][j:j+1])
+                    int(lines[i][j:j+2])
                     
                     e = j + 2
                     # A third num
                     try:
-                        int(lines[i][j:j+2])
+                        int(lines[i][j:j+3])
                         
                         e = j + 3
-                        
                         # triple num result
                         # check the edges on the line (x axis) (x axis)
                         if j == 0:
@@ -144,8 +142,20 @@ def main():
             
             # Check for symbols and add the shiz to the total
             symbol_array = remove_items(symbol_array, ".")
+            for k in nums:
+                symbol_array = remove_items(symbol_array, k)
+
             if len(symbol_array) > 0:
-                sum += lines[i][s:e]
+                print(symbol_array)
+                sum += int(lines[i][s:e])
+                print(f"Number {lines[i][s:e]} added. New sum is {sum}")
+                j = e
+                e = None
+            elif e is not None:
+                j = e
+                e = None
+            else:
+                j += 1
     # Write the answer out to a file
     with open("day_3_output.txt", "w") as o:
         print(f"\nFinal sum: {sum}")
